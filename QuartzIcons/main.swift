19e7ceea5c4a3781e89cde219318919d5fd32db5
Jorge Bernal <jbernal@gmail.com>
2016-11-15 08:56:19 +0100

    Initial commit

---
+++ QuartzIcons/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  QuartzIcons
+//
+//  Created by Jorge Bernal Ordovas on 15/11/2016.
+//  Copyright © 2016 WordPress. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
