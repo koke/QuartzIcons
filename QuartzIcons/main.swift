d304b2894ad359832379e5efe65525f5e4310208
Jorge Bernal <jbernal@gmail.com>
2016-11-15 09:10:18 +0100

    Argument parsing and usage

--- QuartzIcons/main.swift
+++ QuartzIcons/main.swift
@@ -1,12 +1,38 @@
-//
-//  main.swift
-//  QuartzIcons
-//
-//  Created by Jorge Bernal Ordovas on 15/11/2016.
-//  Copyright © 2016 WordPress. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+func usage() {
+    let cmd = (CommandLine.arguments[0] as NSString).lastPathComponent
+    print("Usage: \(cmd) icon_folder framework_name output_dir")
+}
+
+enum Exit: Int32 {
+    case success = 0
+    case invalidArguments
+
+    static func with(_ code: Exit) -> Never {
+        exit(code.rawValue)
+    }
+}
+
+struct Arguments {
+    let input_folder: String
+    let framework_name: String
+    let output_dir: String
+}
+
+func parse_argv(_ argv: [String]) -> Arguments {
+    let arguments = argv.dropFirst()
+    guard arguments.count == 3 else {
+        usage()
+        Exit.with(.invalidArguments)
+    }
+
+    return Arguments(input_folder: arguments[0],
+                     framework_name: arguments[1],
+                     output_dir: arguments[2])
+}
+
+let arguments = parse_argv(CommandLine.arguments)
+
+Exit.with(.success)
 
